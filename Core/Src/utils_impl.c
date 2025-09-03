12bd67503268ab09470a97193de576bef3bcbe1c
ArtyomSkorobogatov <76100659+ArtyomSkorobogatov@users.noreply.github.com>
2025-09-03 16:13:22 +0300

    Add formatter, bash script for RTT start

--- Core/Src/utils_impl.c
+++ Core/Src/utils_impl.c
@@ -8,16 +8,16 @@ char seggerRttUpBuffer[BUFFER_SIZE_UP];
 char seggerRttDownBuffer[BUFFER_SIZE_DOWN];
 #endif
 
-extern TIM_HandleTypeDef htim4;
-
-void utils_init_microseconds_timer() {
-    HAL_TIM_Base_Start(&htim4);
-}
-
-uint32_t utils_get_microseconds_timer() {
-    return TIM4->CNT;
-}
-
-uint32_t utils_get_milliseconds_tick() {
-    return HAL_GetTick();
-}
+// extern TIM_HandleTypeDef htim4;
+//
+// void utils_init_microseconds_timer() {
+//     HAL_TIM_Base_Start(&htim4);
+// }
+//
+// uint32_t utils_get_microseconds_timer() {
+//     return TIM4->CNT;
+// }
+//
+// // uint32_t utils_get_milliseconds_tick() {
+//     return HAL_GetTick();
+// }
