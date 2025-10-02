dbb378182baec4a84a08e38f52668764d68f0b7f
ArtyomSkorobogatov <askor1554@gmail.com>
2025-10-02 18:24:27 +0300

    Add 'run' for 'discrete_io'

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -119,7 +119,8 @@ int main(void) {
     while (1) {
         cdc_process();
         can_process();
-
+        discrete_output_run(&LedRed);
+        discrete_output_run(&LedBlue);
         // If CAN message receive is pending, process the message
         if (is_can_msg_pending(CAN_RX_FIFO0)) {
             // If message received from bus, parse the frame
