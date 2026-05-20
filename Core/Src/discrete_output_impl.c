612c5112cfa6254c8bd5a499ad416f8b14f360d5
ArtyomSkorobogatov <askor1554@gmail.com>
2026-05-20 12:55:14 +0300

    FIX set_pin

--- Core/Src/discrete_output_impl.c
+++ Core/Src/discrete_output_impl.c
@@ -11,7 +11,7 @@ discrete_output_ll_init(const DiscreteOutput_t* pDiscreteOutput) {
 void
 discrete_output_ll_set_level(const DiscreteOutput_t* pDiscreteOutput, const bool level) {
     if (pDiscreteOutput) {
-        GPIO_PinState outputNewState = (level == pDiscreteOutput->Level) ? GPIO_PIN_SET : GPIO_PIN_RESET;
+        GPIO_PinState outputNewState = (level) ? GPIO_PIN_SET : GPIO_PIN_RESET;
         switch (pDiscreteOutput->id) {
             case LED_RED_ID:
                 HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, outputNewState);
